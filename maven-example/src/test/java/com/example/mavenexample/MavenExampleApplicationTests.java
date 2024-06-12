0b6a90f36adee461f5376a2fb2e1dbb8316e568b
Bruce.wu <bruce.wu@optnet.site>
2024-06-12 11:29:43 +0800

    first commit

---
+++ maven-example/src/test/java/com/example/mavenexample/MavenExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavenexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
