f74f83e98cc4f075e1f5f178ed030b45ff69cdbc
Bruce.wu <bruce.wu@optnet.site>
2024-09-08 23:21:48 +0800

    changelog

--- gradle-example/jenkins/changelog-cd.groovy
+++ gradle-example/jenkins/changelog-cd.groovy
@@ -3,6 +3,13 @@ pipeline {
         label 'example'
     }
 
+    parameters {
+        nexusArtifactChoices(name: 'DEPLOY_ARTIFACTS', repository: 'raw-wa', serverId: 'nexus', visibleItemCount: 20,
+                groupIdArtifactIds: '''
+                                tech.bitkernel.devops:gradle-example
+                                ''')
+    }
+
     environment {
         NEXUS_REPO = 'raw-wa'
     }
@@ -11,31 +18,36 @@ pipeline {
         stage('Config') {
             steps {
                 script {
-                    def gav = "${DEPLOY_ARTIFACT}".split(':')
-                    def groupId = gav[0]
-                    def artifactId = gav[1]
-                    def version = gav[2]
+                    def appList = "${DEPLOY_ARTIFACTS}".split(',')
+                    appList.each { app ->
+                        stage("Deploy ${app}") {
+                            script {
+                                def gav = "${app}".split(':')
+                                def groupId = gav[0]
+                                def artifactId = gav[1]
+                                def version = gav[2]
+                                nexusArtifactDownload(
+                                        serverId: 'nexus',
+                                        repository: "${NEXUS_REPO}",
+                                        groupId: groupId,
+                                        artifactId: artifactId,
+                                        version: version,
+                                        location: 'changelog.tar.gz')
 
-                    nexusArtifactDownload(
-                            serverId: 'nexus',
-                            repository: "${NEXUS_REPO}",
-                            groupId: groupId,
-                            artifactId: artifactId,
-                            version: version,
-                            location: 'changelog.tar.gz')
-                    sh '''
-                tar -xvf changelog.tar.gz 
-                '''
+                                sh 'tar -xvf changelog.tar.gz'
 
-                    // 执行Nacos配置变更，读取 changeLog 文件，获取changeSet
-                    def nacosChangeSet = nacosChangeSetGet(nacosId: 'default', changeLogFile: 'changelog/nacos/changelog-root.yaml')
-                    // 弹出界面预览配置前后修改比对
-                    def alterResult = input(message: 'Nacos Config Edit', parameters: [nacosConfigAlter(items: nacosChangeSet['changes'])])
-                    // 应用 changeSet，刷入nacos配置
-                    nacosChangeSetApply(nacosId: "default", changeSetId: nacosChangeSet['id'], items: alterResult['values'])
+                                // 执行Nacos配置变更，读取 changeLog 文件，获取changeSet
+                                def nacosChangeSet = nacosChangeSetGet(nacosId: 'default', changeLogFile: 'changelog/nacos/changelog-root.yaml')
+                                // 弹出界面预览配置前后修改比对
+                                def alterResult = input(message: 'Nacos Config Edit', parameters: [nacosConfigAlter(items: nacosChangeSet['changes'])])
+                                // 应用 changeSet，刷入nacos配置
+                                nacosChangeSetApply(nacosId: "default", changeSetId: nacosChangeSet['id'], items: alterResult['values'])
 
-                    // 执行SQL变更
-                    sh 'liquibase update --defaultsFile=liquibase.properties --changelog-file=changelog/database/changelog-root.yaml'
+                                // 执行SQL变更
+                                sh 'liquibase update --defaultsFile=liquibase.properties --changelog-file=changelog/database/changelog-root.yaml'
+                            }
+                        }
+                    }
                 }
             }
         }
