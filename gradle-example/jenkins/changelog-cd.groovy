beea4883fc62fd25d1722b20c6d8fee0ea1e94cd
Bruce.wu <bruce.wu@optnet.site>
2024-09-09 10:32:49 +0800

    commit

--- gradle-example/jenkins/changelog-cd.groovy
+++ gradle-example/jenkins/changelog-cd.groovy
@@ -4,14 +4,14 @@ pipeline {
     }
 
     parameters {
-        nexusArtifactChoices(name: 'DEPLOY_ARTIFACTS', repository: 'raw-wa', serverId: 'nexus', visibleItemCount: 20,
+        nexusArtifactChoices(name: 'DEPLOY_ARTIFACTS', repository: 'raw-op', serverId: 'nexus', visibleItemCount: 20,
                 groupIdArtifactIds: '''
                                 tech.bitkernel.devops:gradle-example
                                 ''')
     }
 
     environment {
-        NEXUS_REPO = 'raw-wa'
+        NEXUS_REPO = 'raw-op'
     }
 
     stages {
@@ -37,14 +37,20 @@ pipeline {
                                 sh 'tar -xvf changelog.tar.gz'
 
                                 // 执行Nacos配置变更，读取 changeLog 文件，获取changeSet
-                                def nacosChangeSet = nacosChangeSetGet(nacosId: 'default', changeLogFile: 'changelog/nacos/changelog-root.yaml')
-                                // 弹出界面预览配置前后修改比对
-                                def alterResult = input(message: 'Nacos Config Edit', parameters: [nacosConfigAlter(items: nacosChangeSet['changes'])])
-                                // 应用 changeSet，刷入nacos配置
-                                nacosChangeSetApply(nacosId: "default", changeSetId: nacosChangeSet['id'], items: alterResult['values'])
+                                def nacosChangeSet = nacosChangeSetGet(nacosId: 'vod-dev', changeLogFile: 'changelog/nacos/changelog-root.yaml')
 
-                                // 执行SQL变更
-                                sh 'liquibase update --defaultsFile=liquibase.properties --changelog-file=changelog/database/changelog-root.yaml'
+                                if (nacosChangeSet["id"]) {
+                                    // 弹出界面预览配置前后修改比对
+                                    def alterResult = input(message: 'Nacos Config Edit', parameters: [nacosConfigAlter(items: nacosChangeSet['changes'])])
+                                    // 应用 changeSet，刷入nacos配置
+                                    nacosChangeSetApply(nacosId: "vod-dev", changeSetId: nacosChangeSet['id'], items: alterResult['values'])
+                                } else {
+                                    echo "No nacos change set"
+                                }
+
+                                // liquibase update
+                                liquibaseUpdate(databaseId: "optnet-sit", file: "changelog/database/changelog-root.yaml")
+
                             }
                         }
                     }
